6c062ae95eb4ec12d8c3cd409f53fe600073e4a8
Benjamin Mellor <ben@smokingkangaroo.com>
2016-12-08 12:36:25 +1100

    haddock documentation

--- src/Type/Stack.hs
+++ src/Type/Stack.hs
@@ -3,6 +3,27 @@
            , TypeFamilies
            , TypeOperators
   #-}
+{-# OPTIONS_HADDOCK show-extensions #-}
+
+{-|
+Module      : Type.Stack
+Description : Type level list of transformers as a transformer
+Copyright   : © Benjamin Mellor 2016
+License     : BSD-style (see the file LICENSE)
+
+Maintainer  : ben@smokingkangaroo.com
+Stability   : experimental
+Portability : portable
+
+Uses type-level lists to represent a stack of transformers instead of
+nesting them. This can lead to type signatures with fewer parentheses,
+and cleanly separates the transformer stack from the type it is ultimately
+applied to.
+
+This documentation refers to anything of kind @(* -> *) -> (* -> *)@ as a
+"transformer". Most usefully these will be monad transfomers, but not all
+tools in this library require transformers to be instances of 'MonadTrans'.
+-}
 
 module Type.Stack
   (Stack)
@@ -10,6 +31,17 @@ where
 
 import Data.Functor.Identity (Identity)
 
+{-|
+Stacking a list of transfomers is just their composition applied to
+'Identity'.
+
+These types are equivalent:
+
+@
+Stack [MaybeT, ReaderT Int]
+MaybeT (ReaderT Int Identity)
+@
+-}
 type family Stack fs
   where Stack '[] = Identity
         Stack (f : fs) = f (Stack fs)
